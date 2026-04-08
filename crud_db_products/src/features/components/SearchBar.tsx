aaef154d91f354baef498f02b6630d5597fed271
Braulio <braulio.quine@utec.edu.pe>
2026-04-08 17:38:34 -0500

    modifying Search Bar placeholder

--- crud_db_products/src/features/components/SearchBar.tsx
+++ crud_db_products/src/features/components/SearchBar.tsx
@@ -16,7 +16,7 @@ export function SearchBar() {
       <input
         type="search"
         className="filter-control h-14 w-full pl-12 pr-4 text-[1.05rem]"
-        placeholder="Buscar por código, descripción..."
+        placeholder="Buscar por descripción..."
         defaultValue=""
       />
     </label>
